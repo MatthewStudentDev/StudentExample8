a61b2d21fbb80a52aef684938c9218237d72dd96
Matthew <samaham@sheridancollege.ca>
2024-09-24 12:37:57 -0400

    first commit

---
+++ src/main/java/com/samaham/studentexample8/StudentExample8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.samaham.studentexample8;
+
+/**
+ *
+ * @author Matthew Samaha
+ */
+public class StudentExample8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
